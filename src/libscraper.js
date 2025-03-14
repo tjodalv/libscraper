7f2dbe92451f85ed116a586689f59fe04c69a30f
Zvonimir Lokmer <lokmer@gmail.com>
2025-03-14 15:05:26 +0100

    formatters extracted into their own file and imported in libscraper

--- src/libscraper.js
+++ src/libscraper.js
@@ -2,8 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import https from 'https';
 import * as cheerio from 'cheerio';
-import { createObjectCsvWriter } from 'csv-writer';
 import { extension as getExtensionFromMimeType } from 'mime-types';
+import { csvFormatter, jsonFormatter } from './formatters';
 
 const defaultOptions = {
     format: 'json',
@@ -45,60 +45,8 @@ const Scraper = {
     },
 
     _dataFormatters: {
-        json: async function jsonFormatter(filepath, data) {
-            if (data.length === 0) {
-                console.log('No file saved, data is empty');
-                return false;
-            }
-
-            const fsPromise = fs.promises;
-
-            if (! path.extname(filepath)) {
-                filepath += '.json';
-            }
-
-            try {
-                // Convert the data array to a JSON string with indentation
-                const jsonString = JSON.stringify(data, null, 4);
-
-                // Write the JSON data to the file
-                await fsPromise.writeFile(filepath, jsonString, 'utf8');
-
-                return filepath;
-            } catch (error) {
-                console.error("Error writing JSON file:", error);
-            }
-
-            return false;
-        },
-        csv: async function csvFormatter(filepath, data) {
-            if (data.length === 0) {
-                console.log('No file saved, data is empty');
-                return false;
-            }
-
-            if (! path.extname(filepath)) {
-                filepath += '.csv';
-            }
-
-            // Get all unique keys to ensure all attributes are included in CSV headers
-            const keys = new Set(data.flatMap(obj => Object.keys(obj)));
-
-            try {
-                const csvWriter = createObjectCsvWriter({
-                    path: filepath,
-                    header: Array.from(keys).map(key => ({ id: key, title: key }))
-                });
-
-                await csvWriter.writeRecords(data);
-
-                return filepath;
-            } catch (error) {
-                console.error("Error writing CSV file:", error);
-            }
-
-            return false;
-        }
+        json: jsonFormatter,
+        csv: csvFormatter,
     },
 
     _fetched_pages_num: 0,
